0bb8396de0df6ecf8927c2839c62237f109ff965
vineetsouri <vineetsouri72@gmail.com>
2015-05-30 18:30:04 +0530

    files for the todoapp

---
+++ js/todo.js
@@ -0,0 +1,20 @@
+function TodoCtrl($scope) {
+  
+  $scope.todos = [];
+  
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  };
+  
+  
+  $scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.formTodoText = '';
+  };
+  
+    $scope.clearCompleted = function () {
+        $scope.todos = $scope.todos.filter(function(todo){
+            return !todo.done;
+        });
+    };
+}
